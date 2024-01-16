037f6a443eebe948149cbc90d6348e95238999d0
Alexislepetit <alexis.lepetit@outlook.fr>
2024-01-16 11:13:57 +0100

    Modif2 chemin acces

--- SOUMSOUM Version 4/Application_Logistique.py
+++ SOUMSOUM Version 4/Application_Logistique.py
@@ -226,7 +226,7 @@ class StockUpdaterGUI:
 
 
         else:      
-            subprocess.Popen([sys.executable, 'SOUMSOUM Version 4/Page_de_connection.py'])
+            subprocess.Popen([sys.executable, 'Page_de_connection.py'])
             
 def main():
     root = tk.Tk()
