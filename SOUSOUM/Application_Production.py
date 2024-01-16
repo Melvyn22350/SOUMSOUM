6be70bcad93b04838abf635b9ea65ab2ea2affdb
D3rKSouL <155458131+D3rKSouL@users.noreply.github.com>
2024-01-16 12:05:03 +0100

    Update Application_Production.py

--- SOUSOUM/Application_Production.py
+++ SOUSOUM/Application_Production.py
@@ -93,101 +93,122 @@ class OdooAPI:
 
         if uid is not None:
             self.models = xmlrpc.client.ServerProxy('{}/xmlrpc/2/object'.format(self.url))
-
             root = tk.Tk()
-            root.title("Informations et Modification des Ordres de Fabrication - Odoo")
+            self.configure_root(root)
 
-            root.configure(bg='#3498db')
+            tree = self.create_treeview(root)
+            self.display_treeview(tree)
 
-            tree = ttk.Treeview(root, columns=("Ordre ID", "Réference Produit", "Date prévue", "Quantité Produite", "Stock Produit", self.status_column), show="headings")
-            tree.heading("Ordre ID", text="Ordre ID", anchor="center", command=lambda: self.sort_treeview(tree, "Ordre ID"))
-            tree.heading("Réference Produit", text="Réference Produit", anchor="center", command=lambda: self.sort_treeview(tree, "Réference Produit"))
-            tree.heading("Date prévue", text="Date prévue", anchor="center", command=lambda: self.sort_treeview(tree, "Date prévue"))
-            tree.heading("Quantité Produite", text="Quantité Produite", anchor="center", command=lambda: self.sort_treeview(tree, "Quantité Produite"))
-            tree.heading("Stock Produit", text="Stock Produit", anchor="center", command=lambda: self.sort_treeview(tree, "Stock Produit"))
-            tree.heading(self.status_column, text=self.status_column, anchor="center", command=lambda: self.sort_treeview(tree, self.status_column))
-
-            for col in ("Ordre ID", "Réference Produit", "Date prévue", "Quantité Produite", "Stock Produit", self.status_column):
-                tree.column(col, anchor="center")
-
-            order_ids = self.get_order_ids()
-            for order_id in order_ids:
-                order_info = self.get_order_info(order_id)
-                if order_info:
-                    order_info = order_info[0]
-                    product_ref = order_info.get('product_id', '')
-                    date_planned = order_info.get('date_planned_start', '')
-                    qty_producing = order_info.get('qty_producing', '')
-                    product_qty = order_info.get('product_qty', '')
-                    order_status = order_info.get('state', '')
-
-                    today = datetime.datetime.today().strftime('%Y-%m-%d')
-                    date_style = 'green' if date_planned >= today else 'red'
-
-                    tree.insert("", "end", iid=order_id, values=(order_id, product_ref, date_planned, qty_producing, product_qty, order_status), tags=(date_style,))
-
-            tree.tag_configure('red', foreground='red')
-            tree.tag_configure('green', foreground='green')
-
-            tree.pack(padx=10, pady=10)
-
-            id_label = tk.Label(root, text="Rentrer l'ID de l'ordre de fabrication afin de modifier la quantité produite :", fg="white", font=("Helvetica", 12, "bold"), highlightthickness=0, bg='#3498db')
-            id_label.pack(pady=10)
-
-            id_entry = tk.Entry(root)
-            id_entry.pack(pady=10)
-
-            quantity_produced_label = tk.Label(root, text="Quantité produite actuelle :", fg="white", font=("Helvetica", 12, "bold"),
-            highlightthickness=0, background='#3498db')
-            quantity_produced_label.pack(pady=10)
-
-            quantity_produced_entry = tk.Entry(root)
-            quantity_produced_entry.pack(pady=10)
-
-            style = ttk.Style()
-            style.configure("TButton", font=("Helvetica", 12, "bold"))
-
-            style.map("Red.TButton",
-                    foreground=[('pressed', 'white'), ('active', 'white')],
-                    background=[('pressed', 'red'), ('active', 'red')])
-
-            style.map("Green.TButton",
-                    foreground=[('pressed', 'white'), ('active', 'white')],
-                    background=[('pressed', 'green'), ('active', 'green')])
-
-            update_button = ttk.Button(root, text="Mettre à jour la quantité produite", command=lambda: self.update_quantity_produced(id_entry.get(), quantity_produced_entry.get(), tree), style="Green.TButton", cursor="hand2")
-            update_button.pack(pady=60)
-
-            restart_button = ttk.Button(root, text="Actualiser la page", command=self.restart_program, style="Green.TButton", cursor="hand2")
-            restart_button.pack(side="left", padx=30, pady=30)
-
-            quit_button = ttk.Button(root, text="Déconnexion", command=lambda: self.quit_program(root), style="Red.TButton", cursor="hand2")
-            quit_button.pack(side="right", padx=30, pady=30)
-
+            id_label, id_entry, quantity_produced_label, quantity_produced_entry, style = self.create_user_interface(root, tree)
+            self.create_buttons(root, tree, id_entry, quantity_produced_entry, style)
+            
             root.protocol("WM_DELETE_WINDOW", lambda: self.close_program(root))
             self.set_icon(root)
             self.root = root
-
-            def center_window():
-                root.update_idletasks()
-                screen_width = root.winfo_screenwidth()
-                screen_height = root.winfo_screenheight()
-                x_position = (screen_width - root.winfo_reqwidth()) // 2
-                y_position = (screen_height - root.winfo_reqheight()) // 2
-                root.geometry("+{}+{}".format(x_position, y_position))
-
-            center_window()
+            self.center_window(root)
 
             root.mainloop()
         else:
             print("Échec de l'authentification.")
 
+    def configure_root(self, root):
+        root.title("Informations et Modification des Ordres de Fabrication - Odoo")
+        root.configure(bg='#3498db')
+
+    def create_treeview(self, root):
+        tree = ttk.Treeview(root, columns=("Ordre ID", "Réference Produit", "Date prévue", "Quantité Produite", "Stock Produit", self.status_column), show="headings")
+        self.configure_treeview(tree)
+        return tree
+
+    def configure_treeview(self, tree):
+        tree.heading("Ordre ID", text="Ordre ID", anchor="center", command=lambda: self.sort_treeview(tree, "Ordre ID"))
+        tree.heading("Réference Produit", text="Réference Produit", anchor="center", command=lambda: self.sort_treeview(tree, "Réference Produit"))
+        tree.heading("Date prévue", text="Date prévue", anchor="center", command=lambda: self.sort_treeview(tree, "Date prévue"))
+        tree.heading("Quantité Produite", text="Quantité Produite", anchor="center", command=lambda: self.sort_treeview(tree, "Quantité Produite"))
+        tree.heading("Stock Produit", text="Stock Produit", anchor="center", command=lambda: self.sort_treeview(tree, "Stock Produit"))
+        tree.heading(self.status_column, text=self.status_column, anchor="center", command=lambda: self.sort_treeview(tree, self.status_column))
+
+        for col in ("Ordre ID", "Réference Produit", "Date prévue", "Quantité Produite", "Stock Produit", self.status_column):
+            tree.column(col, anchor="center")
+
+    def display_treeview(self, tree):
+        order_ids = self.get_order_ids()
+        for order_id in order_ids:
+            order_info = self.get_order_info(order_id)
+            if order_info:
+                order_info = order_info[0]
+                product_ref = order_info.get('product_id', '')
+                date_planned = order_info.get('date_planned_start', '')
+                qty_producing = order_info.get('qty_producing', '')
+                product_qty = order_info.get('product_qty', '')
+                order_status = order_info.get('state', '')
+
+                today = datetime.datetime.today().strftime('%Y-%m-%d')
+                date_style = 'green' if date_planned >= today else 'red'
+
+                tree.insert("", "end", iid=order_id, values=(order_id, product_ref, date_planned, qty_producing, product_qty, order_status), tags=(date_style,))
+
+        tree.tag_configure('red', foreground='red')
+        tree.tag_configure('green', foreground='green')
+
+        tree.pack(padx=10, pady=10)
+
+    def create_user_interface(self, root, tree):
+        id_label = tk.Label(root, text="ID de l'ordre de fabrication :", fg="white", font=("Helvetica", 12, "bold"), highlightthickness=0, bg='#3498db')
+        id_label.pack(pady=10)
+
+        id_entry = tk.Entry(root)
+        id_entry.pack(pady=10)
+
+        quantity_produced_label = tk.Label(root, text="Quantité produite actuelle :", fg="white", font=("Helvetica", 12, "bold"),
+        highlightthickness=0, background='#3498db')
+        quantity_produced_label.pack(pady=10)
+
+        quantity_produced_entry = tk.Entry(root)
+        quantity_produced_entry.pack(pady=10)
+
+        style = ttk.Style()
+        style.configure("TButton", font=("Helvetica", 12, "bold"))
+        
+        style.map("Red.TButton",
+                foreground=[('pressed', 'white'), ('active', 'white')],
+                background=[('pressed', 'red'), ('active', 'red')])
+
+        style.map("Green.TButton",
+                foreground=[('pressed', 'white'), ('active', 'white')],
+                background=[('pressed', 'green'), ('active', 'green')])
+
+        return id_label, id_entry, quantity_produced_label, quantity_produced_entry, style
+
+    def create_buttons(self, root, tree, id_entry, quantity_produced_entry, style):
+        update_button = ttk.Button(root, text="Mettre à jour la quantité produite", command=lambda: self.update_quantity_produced(id_entry.get(), quantity_produced_entry.get(), tree), style="Green.TButton", cursor="hand2")
+        update_button.pack(pady=60)
+
+        restart_button = ttk.Button(root, text="Actualiser la page", command=self.restart_program, style="Green.TButton", cursor="hand2")
+        restart_button.pack(side="left", padx=30, pady=30)
+
+        quit_button = ttk.Button(root, text="Déconnexion", command=lambda: self.quit_program(root), style="Red.TButton", cursor="hand2")
+        quit_button.pack(side="right", padx=30, pady=30)
+
+    def center_window(self, root):
+        root.update_idletasks()
+        screen_width = root.winfo_screenwidth()
+        screen_height = root.winfo_screenheight()
+        x_position = (screen_width - root.winfo_reqwidth()) // 2
+        y_position = (screen_height - root.winfo_reqheight()) // 2
+        root.geometry("+{}+{}".format(x_position, y_position))
+
+    
+#====================================================================
+#====================================================================
     def set_icon(self, root):
         url = "http://172.31.11.79:8069"
         db = "SOUMSOUM"
         username = "melvyndupas01@gmail.com"
         password = "123456789"
-
+#====================================================================
+#====================================================================
+        
         common = xmlrpc.client.ServerProxy('{}/xmlrpc/2/common'.format(url))
         uid = common.authenticate(db, username, password, {})
         
@@ -231,8 +252,12 @@ class OdooAPI:
     def redirect_to_login(self, root):
         print("Redirection vers la page de connexion.")
         root.destroy()
+#====================================================================
+#====================================================================
         subprocess.Popen([sys.executable, '/home/user/Documents/SOUMSOUM/Programme/Page_de_connection.py'])
-
+#====================================================================
+#====================================================================
+    
     def sort_treeview(self, tree, column):
         items = [(tree.set(k, column), k) for k in tree.get_children('')]
         items.sort()
@@ -240,6 +265,10 @@ class OdooAPI:
             tree.move(k, '', index)
         tree.heading(column, command=lambda: self.sort_treeview(tree, column))
 
+#====================================================================
+#====================================================================
 if __name__ == "__main__":
     odoo_api = OdooAPI('http://172.31.11.79:8069', 'SOUMSOUM', 'melvyndupas01@gmail.com', '123456789')
     odoo_api.display_and_modify_orders()
+#====================================================================
+#====================================================================
