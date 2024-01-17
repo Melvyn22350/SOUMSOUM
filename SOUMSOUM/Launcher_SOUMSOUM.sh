50d6d3550f3a2528fd5c97fa0accf308e90e2867
Alexislepetit <155457901+Alexislepetit@users.noreply.github.com>
2024-01-17 10:07:58 +0100

    Update Launcher_SOUMSOUM.sh

--- SOUMSOUM/Launcher_SOUMSOUM.sh
+++ SOUMSOUM/Launcher_SOUMSOUM.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 
-python3 Start.py
+# Récupérer le répertoire du script
+script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Utiliser le chemin complet pour lancer Start.py
+python3 "$script_directory/SOUMSOUM_Software/Start.py"
