7e4502634b28a03192f66e956b5a4420c147a850
Alexislepetit <155457901+Alexislepetit@users.noreply.github.com>
2024-01-17 11:23:41 +0100

    Delete SOUMSOUM/Launcher_SOUMSOUM.sh

--- SOUMSOUM/Launcher_SOUMSOUM.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Récupérer le répertoire du script
-script_directory="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-
-# Utiliser le chemin complet pour lancer Start.py
-python3 "$script_directory/SOUMSOUM_Software/Start.py"
